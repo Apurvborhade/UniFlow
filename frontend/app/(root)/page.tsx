f4cfda6cbf23ff3cc06ac1fcf620ea2f8008840a
Ayush sonar <ayushsonar255@gmail.com>
2026-02-05 12:59:36 +0530

    Monthly inflow card UI

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -3,7 +3,7 @@ import BalanceTrendsChart from "@/components/BalanceTrendsChart";
 const Dashboard = () => {
   return (
     <div>
-      <section className="max-w-7xl  mx-auto px-5 py-4 flex flex-col items-center justify-between">
+      <section className="max-w-5xl  mx-auto px-5 py-4 flex flex-col items-center justify-between">
         <div className="bg-white w-full border border-black rounded-xl p-6 mb-8 pt-6 pb-10">
           <h2 className="text-black font-medium text-lg mb-4">
             Total Treasury
@@ -40,14 +40,15 @@ const Dashboard = () => {
 
           {/* Right Column Stats */}
           <div className="flex flex-col justify-between gap-6">
-            <div className="bg-white border h-full border-gray-700 rounded-xl p-6">
-              <h3 className="text-black font-semibold mb-4">
-                Monthly inflow
-              </h3>
+            <div className="bg-white border flex-2 h-full border-gray-700 rounded-xl p-6">
+              <h3 className="text-black text-lg font-semibold mb-4">Monthly inflow</h3>
+              <p className="text-3xl font-bold text-black">$44,000</p>
+              <p className="text-[#26FF00] pt-3 text-[20px]  font-bold mt-2">
+                ↑ 12% from last month
+              </p>
             </div>
 
-           
-            <div className="bg-white border h-full  border-gray-700 rounded-xl p-6">
+            <div className="bg-white border  flex-1   h-full  border-gray-700 rounded-xl p-6">
               <h3 className="text-black font-semibold mb-4">Current APY</h3>
             </div>
           </div>
