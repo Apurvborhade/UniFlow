48fa8d13b1e403331d91b7f1cb59f911fc1c75da
Ayush sonar <ayushsonar255@gmail.com>
2026-02-05 11:44:32 +0530

    Font size and textColor changes on numbers

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -4,7 +4,7 @@ const Dashboard = () => {
       <div className="bg-white w-full border border-black rounded-xl p-6 mb-8 pt-6 pb-10">
         <h2 className="text-black font-medium text-lg mb-4">Total Treasury</h2>
         <div className="mb-6">
-          <p className="text-4xl font-bold text-gray-900">$657,500</p>
+          <p className="text-5xl font-extrabold text-black">$657,500</p>
           <p className="text-gray-600 text-[17px] mt-1">USDC Balance</p>
         </div>
         <hr className="my-6 border-b-gray-400" />
@@ -13,7 +13,7 @@ const Dashboard = () => {
             <p className="text-gray-600 text-[17px] mb-2">
               Available for Deployment
             </p>
-            <p className="text-2xl font-bold text-gray-900">$125,500</p>
+            <p className="text-2xl font-bold text-black">$125,500</p>
           </div>
           <div className="text-right">
             <p className="text-gray-600 text-[17px] mb-2">Reserved for Payroll</p>
